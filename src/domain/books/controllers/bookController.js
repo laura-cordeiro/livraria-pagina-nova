a2c35f7ddde6d873749053f3026703044faf064d
Laura Cordeiro <92588463+laura-cordeiro@users.noreply.github.com>
2022-07-03 18:15:30 -0300

    adicionando cloudinary upload

--- src/domain/books/controllers/bookController.js
+++ src/domain/books/controllers/bookController.js
@@ -1,13 +1,17 @@
-const { Books } = require("../models");
-imageFolder = "images/";
+const { Books } = require("../models"),
+  imageFolder = "images/",
+  cloudinary = require("../../../config/cloudinary");
 
 const BookController = {
   async create(req, res) {
     const file = req.files[0];
+    const uploadPath = await cloudinary.uploads(file.path, "Books");
 
     const newBook = await Books.create({
       ...req.body,
-      image: imageFolder + file.filename
+      //forma de upload na aplicação
+      // image: imageFolder + file.filename
+      image: uploadPath.imageUrl
     });
 
     return res.status(201).json({ dadosFinais: newBook });
